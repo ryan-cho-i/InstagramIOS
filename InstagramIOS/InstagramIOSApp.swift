bce7d3b094258ed9232b2ec91be0fe1b13c688cc
ryan-cho-i <chlqudtn347@gmail.com>
2023-10-25 16:54:42 -0400

    Initial Commit

---
+++ InstagramIOS/InstagramIOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  InstagramIOSApp.swift
+//  InstagramIOS
+//
+//  Created by soo on 10/25/23.
+//
+
+import SwiftUI
+
+@main
+struct InstagramIOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

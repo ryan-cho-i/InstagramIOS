db382e72b7142218f5c8f44b833537e909c4c6a3
ryan-cho-i <chlqudtn347@gmail.com>
2023-10-26 11:28:57 -0400

    Post Data Model

---
+++ InstagramIOS/Core/Components/PostGridView.swift
@@ -0,0 +1,18 @@
+//
+//  PostGridView.swift
+//  InstagramIOS
+//
+//  Created by soo on 10/26/23.
+//
+
+import SwiftUI
+
+struct PostGridView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    PostGridView()
+}
---
+++ InstagramIOS/Model/Post.swift
@@ -0,0 +1,8 @@
+//
+//  Post.swift
+//  InstagramIOS
+//
+//  Created by soo on 10/26/23.
+//
+
+import Foundation

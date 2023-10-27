0b725439b5f3cb2060bfb81074df515cfa7da306
ryan-cho-i <chlqudtn347@gmail.com>
2023-10-27 17:35:23 -0400

    Loading Images

---
+++ InstagramIOS/Core/Components/CircularProfileImageView.swift
@@ -0,0 +1,18 @@
+//
+//  CircularProfileImageView.swift
+//  InstagramIOS
+//
+//  Created by soo on 10/27/23.
+//
+
+import SwiftUI
+
+struct CircularProfileImageView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CircularProfileImageView()
+}
---
+++ InstagramIOS/Services/ImageUploader.swift
@@ -0,0 +1,8 @@
+//
+//  ImageUploader.swift
+//  InstagramIOS
+//
+//  Created by soo on 10/27/23.
+//
+
+import Foundation
